16842af2422cc2f861511523edbd34d85210f1c4
Richard Obiri <obiriaframson@gmail.com>
2025-04-29 20:11:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import os
+
 def call_me():
-  print("Calling me ")
+    # Read the environment variable CALL_ME
+    call_me_key = os.environ.get("CALL_ME")
+    if call_me_key is None:
+        print("Environment variable CALL_ME is not set.")
+    else:
+        print(f"CALL_ME = {call_me_key}")
+
+if __name__ == "__main__":
+    call_me()
