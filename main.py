b9982caa9be32757a43d2005244bff0d073c61ae
Richard Obiri <obiriaframson@gmail.com>
2025-06-16 02:00:49 +0000

    Update user script

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def call_me():
     if not api_key:
         print("Environment variable CALL_ME is not set.")
     else:
-        print(f"CALL_ME = {api_key}")
+        print(f"CALL_ME = the key exist no problem at all")
 
 if __name__ == "__main__":
     call_me()
